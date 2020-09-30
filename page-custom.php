a5b9133f0d787c4b0af793e44166a29b6093db1d
Giulia Mummolo <mail@giuliamummolo.com>
2020-09-30 16:54:35 +0100

    added animation

--- page-custom.php
+++ page-custom.php
@@ -6,13 +6,14 @@
   while ( have_posts() ) : the_post();
 ?>
 
-<div class='page-content-wrapper'>
+<div class='page-custom-content-wrapper'>
 
   <h1><?php the_title(); ?></h1>
 
   <hr>
 
   <?php the_content(); ?>
+
 
   <hr>
   <h3>.gm</h3>
