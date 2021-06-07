051eab4b642fd7dfbb55397bb670e7703855406d
Giulia Mummolo <mail@giuliamummolo.com>
2021-06-07 18:24:15 +0100

    added social links

--- footer.php
+++ footer.php
@@ -7,8 +7,9 @@
 
     <div class='footer-menu'>
     <img src="<?php echo get_bloginfo('template_url') ?>/media/logo-secondary-light.png"/> 
-
     </div>
+
+  
 
 
   </div>
@@ -16,6 +17,13 @@
   <?php echo wp_nav_menu('Main Menu'); ?>
 
   <div class='theme-link'> 
+
+  <div class='social-icons'>
+   <a href='https://www.instagram.com/thedecoraedit/'><img src="<?php echo get_bloginfo('template_url') ?>/media/instagram-logo.png"/> </a>
+   <a href='https://www.youtube.com/channel/UCErO2r8Y-p0w9lpnxNf8gTg'> <img src="<?php echo get_bloginfo('template_url') ?>/media/youtube-logo.png"/> </a>
+   <a href='https://www.pinterest.co.uk/thedecoraedit/'> <img src="<?php echo get_bloginfo('template_url') ?>/media/logo-pinterest.png"/> </a>
+
+    </div>
     <p id='theme-link'><a href='https://github.com/giuliamml/WP_customtemplate'>theme</a> designed and built by GIULIA MUMMOLO </p> 
   </div>
 
